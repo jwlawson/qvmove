1a416763ba2e14d4cccae5716cb84ffab28803f2
John Lawson <john@jwlawson.co.uk>
2014-12-03 17:06:05 +0000

    Adding hcs class reps

--- src/const.cc
+++ src/const.cc
@@ -21,32 +21,16 @@ namespace {
 using namespace cluster::mmi_conn;
 
 std::vector<std::shared_ptr<cluster::MMIMove>> Moves = {
-	/* Sink source */
-	make_move("{ { 0 -1 } { 1 0 } }", "{ { 0 1 } { -1 0 } }",
-			{ 1 }, { Unconnected() }),
-	make_move("{ { 0 1 0 } { -1 0 -1 } { 0 1 0 } }",
-			"{ { 0 -1 0 } { 1 0 1 } { 0 -1 0 } }",
-			{ 0, 2 }, { ConnectedTo(2), ConnectedTo(0) }),
-	make_move("{ { 0 1 1 1 } { -1 0 0 0 } { -1 0 0 0 } { -1 0 0 0 } }",
-			"{ { 0 -1 -1 -1 } { 1 0 0 0 } { 1 0 0 0 } { 1 0 0 0 } }",
-			{1, 2, 3}, {ConnectedTo(2, 3), ConnectedTo(1, 3), ConnectedTo(1, 2)}),
-	make_move("{ { 0 1 1 1 1 } { -1 0 0 0 0 } { -1 0 0 0 0 } { -1 0 0 0 0 } "
-			"{ -1 0 0 0 0 } }",
-			"{ { 0 -1 -1 -1 -1 } { 1 0 0 0 0 } { 1 0 0 0 0 } { 1 0 0 0 0 } "
-			"{ 1 0 0 0 0 } }",
-			{1, 2, 3, 4}, { ConnectedTo({2, 3, 4}), ConnectedTo({1, 3, 4}),
-				ConnectedTo({1, 2, 4}), ConnectedTo({1, 2, 3}) }),
-	make_move("{ { 0 1 1 1 1 1 } { -1 0 0 0 0 0 } { -1 0 0 0 0 0 } "
-			"{ -1 0 0 0 0 0 } { -1 0 0 0 0 0 } { -1 0 0 0 0 0 } }",
-			"{ { 0 -1 -1 -1 -1 -1 } { 1 0 0 0 0 0 } { 1 0 0 0 0 0 } { 1 0 0 0 0 0 } "
-			"{ 1 0 0 0 0 0 } { 1 0 0 0 0 0 } }",
-			{1, 2, 3, 4, 5}, { ConnectedTo({2, 3, 4, 5}), ConnectedTo({1, 3, 4, 5}),
-				ConnectedTo({1, 2, 4, 5}), ConnectedTo({1, 2, 3, 5}),
-				ConnectedTo({1, 2, 3, 4}) })
 };
 
 std::unordered_set<std::shared_ptr<cluster::EquivQuiverMatrix>> Reps = {
-	matrix("{ { 0 1 0 0 } { -1 0 1 1 } { 0 -1 0 1 } { 0 -1 -1 0 } }")
+	/* 4 x 4 Hyperbolic coxeter diagram orientations */
+	matrix("{ { 0 1 0 0 } { -1 0 1 1 } { 0 -1 0 1 } { 0 -1 -1 0 } }"),
+	matrix("{ { 0 1 1 -1 } { -1 0 -1 0 } { -1 1 0 -1 } { 1 0 1 0 } }"),
+	matrix("{ { 0 1 1 1 } { -1 0 -1 0 } { -1 1 0 -1 } { -1 0 1 0 } }"),
+	matrix("{ { 0 1 1 1 } { -1 0 -1 1 } { -1 1 0 1 } { -1 -1 -1 0 } }"),
+	matrix("{ { 0 1 1 -1 } { -1 0 -1 1 } { -1 1 0 1 } { 1 -1 -1 0 } }"),
+	matrix("{ { 0 1 1 1 } { -1 0 1 -1 } { -1 -1 0 1 } { -1 1 -1 0 } }")
 };
 
 }
