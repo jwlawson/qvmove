08088b083b0a8ae8123d4a966527af28a9f7c3c2
John Lawson <john@jwlawson.co.uk>
2015-01-22 14:52:45 +0000

    Adds copyright notices.

--- src/const.cc
+++ src/const.cc
@@ -1,5 +1,25 @@
 /**
  * consts.cc
+ * Copyright 2014-2015 John Lawson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Contains all the class representatives and all moves.
+ *
+ * This just initialises the data structures used by the program to check
+ * moves. There are some helper functions to create the moves, matrices and
+ * graphs.
  */
 #include "consts.h"
 
